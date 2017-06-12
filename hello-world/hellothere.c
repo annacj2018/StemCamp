431824a31bd5e0d289e062f326fc688df99daea8
Anna Jackson <annacj2018@gmail.com>
2017-06-12 11:20:57 -0500

    Initial Commit

---
+++ hello-world/hellothere.c
@@ -0,0 +1,14 @@
+/* 
+ * Anna Jackson
+ * Program #1
+ * hello world
+ * 06/12/17
+*/
+#include "simpletools.h"
+
+int main()                                    // Main function
+{
+  print("hello world \n");
+  print("UNA Stem Camp");
+  
+}
