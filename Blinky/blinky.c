d455dc2e5cd88915cf2eb21688cb110710626cbd
Anna Jackson <annacj2018@gmail.com>
2017-06-12 11:22:15 -0500

    Initial Commit

---
+++ Blinky/blinky.c
@@ -0,0 +1,32 @@
+/*
+  *Anna Jackson
+  *LED Blink Project 
+  *Program #2
+  *06/12/17
+*/
+#include "simpletools.h"                      // Include simple tools
+
+int main()                                    // Main function
+{
+ 
+
+ 
+  while(1)
+  {
+    high(0); 
+    pause(250);
+    low(0); 
+    pause(150);
+    
+    high(15);
+    pause(250);
+    low(15);
+    pause(150);
+    
+    
+   
+    
+  
+  } 
+       
+}
