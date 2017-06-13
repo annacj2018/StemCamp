d2adcc445390038a17bdd2eea4505bf1d092ab4c
Anna Jackson <annacj2018@gmail.com>
2017-06-13 09:54:38 -0500

    Initial commit

---
+++ counter/Counter.c
@@ -0,0 +1,23 @@
+/*
+  *Project #3
+  *Counter
+  *Anna Jackson
+  *06/12/17
+*/
+#include "simpletools.h"                      
+
+int main()                                    
+{
+  int  x=28;
+ 
+  while(x < 128)
+  { 
+    print("%d\n", x);
+    x = x + 1 ;
+   
+ 
+    
+    
+  }
+  print("done");  
+}
